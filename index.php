77d5d93e2f7909e0df5722d30059bfc6f79a30cd
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-03-02 22:53:24 -0800

    リスト画面成型

--- index.php
+++ index.php
@@ -22,16 +22,43 @@ $questionList = json_decode($jsonString, true);
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.35.0/min/vs/editor/editor.main.css"
     />
+    <style>
+  .row:hover {
+    background-color: #f2f2f2; /* Change the color as needed */
+  }
+</style>
   </head>
 
   <body>
 
-    <ul>
-    <?php foreach ($questionList as $question) : ?>
-        <li><a href="question.php?id=<?= $question['id'] ?>"><?=  $question['title']?></a></li>
+  <main style="max-width: 800px; margin: auto;">
+  <div style="text-align:center; margin-top:20px;">
+
+    <h1>PlantUML 練習サイト</h1>
+  </div>
+    <table style="width:100%; text-align:center; border: 1px solid #f2f2f2; margin-top: 50px;">
+      <tr style="background-color:aqua;">
+        <th>ID</th>
+        <th>Title</th>
+    <th>Theme</th>
+    
+  </tr>
+  <?php foreach ($questionList as $question) : ?>
+   
+    <tr class="row" window.location.href = 'question.php?id=<?= $question['id'] ?>'">
+        <td> <?= $question['id'] ?></td>
+        <td><?= $question['title'] ?></td>
+        <td><?= $question['theme'] ?></td>
+        
+        
+      </tr>
+      
     <?php endforeach; ?>
-
-
-    </ul>
-  </body>
+    
+    
+    
+  </table>
+  
+</main>
+</body>
 </html>
